f935a488511b6f37bd77355a493b523e4abfe98a
FriendWav <134782240+BludnyHolandan@users.noreply.github.com>
2025-02-12 23:05:17 +0100

    Update main.lua

--- v2/autofarm/main.lua
+++ v2/autofarm/main.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/104dda0f15fa452ef6dcba44.lua'))()
+script_key = "SVGIXJKglDLsNCIQcahntyqsHeGHuemn";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b22aae7d03041699ada62f6a4fb519fd.lua"))()
